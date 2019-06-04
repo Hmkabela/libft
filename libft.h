2ae3f776ce448e34ad73d0b43b8237f37f1a31d0
Hlamalani MKABELA <hmkabela@c4r11s8.wethinkcode.co.za>
2019-06-04 18:37:52 +0200

    .

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hmkabela <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 10:08:23 by hmkabela          #+#    #+#             */
-/*   Updated: 2019/06/03 16:13:34 by hmkabela         ###   ########.fr       */
+/*   Updated: 2019/06/04 17:21:37 by hmkabela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@
 //int ft_memcmp(const void *s1, const void *s2, size_t n);
 //size_t	ft_strlen(const char *s);
 //char	*ft_strdup(const char *s1);
-//char	*ft_strcpy(char *dst, const char *src);
-//char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
 //char	*ft_strcat(char *restrict s1, const char *restrict s2);
 //char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
 //size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
