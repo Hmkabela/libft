735a3f45b17fd601c65d93c250a438271a2f91aa
Hlamalani MKABELA <hmkabela@c4r11s8.wethinkcode.co.za>
2019-06-12 18:55:38 +0200

    .

--- main.c
+++ main.c
@@ -1,52 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmkabela <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/12 17:27:36 by hmkabela          #+#    #+#             */
-/*   Updated: 2019/06/12 18:55:12 by hmkabela         ###   ########.fr       */
+/*   Created: 2019/06/12 17:40:03 by hmkabela          #+#    #+#             */
+/*   Updated: 2019/06/12 18:30:39 by hmkabela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+int	main()
 {
-	char *str;
-	int cc;
-	int num;
-	int a;
-	int s;
-
-	num = n;
-	cc = 1;
-	s = -1;
-	if (num <= 9)
-		num *= s;
-	while (num > 9)
-	{
-		num /= 10;
-		cc++;
-	}
-	num = n;
-	if (n >= 0)
-	{
-		str = ft_strnew(cc);
-		a = 0;
-	}
-	else
-	{
-		str = ft_strnew(cc + 1);
-		a = 1;
-		num *= s;
-	}
-	while (cc > a)
-	{
-		cc--;
-		str[cc] = (num % 10) + 48;
-		num /= 10;
-	}
-	return (str);
+	ft_putstr(ft_itoa(-8));
+	return (0);
 }
